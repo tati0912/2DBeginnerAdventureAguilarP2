7a9d0836b325d83310f2a04ffb3b7ccf3ee7c68c
tati0912 <730124@student.dusd.net>
2024-11-18 10:19:14 -0800

    need to finidh 5th video
    finidh 5th video

--- 2DAdventureAguilarP2/Assets/Scripts/DuckoController.cs
+++ 2DAdventureAguilarP2/Assets/Scripts/DuckoController.cs
@@ -4,24 +4,33 @@ using UnityEngine;
 
 public class DuckoController : MonoBehaviour
 {
-    public float speed = 4.0f;
+    Rigidbody2D rigidbody2d;
+    float horizontal;
+    float vertical;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
+    }
 
-        Vector2 position = transform.position;
-        position.x = position.x + speed * horizontal * Time.deltaTime;
-        position.y = position.y + speed * vertical * Time.deltaTime;
 
-        transform.position = position;
+
+
+
+    void FixedUpdate()
+    {
+        Vector2 position = rigidbody2d.position;
+        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
+        position.y = position.y + 3.0f * vertical * Time.deltaTime;
+
+        rigidbody2d.MovePosition(position);
     }
 }
